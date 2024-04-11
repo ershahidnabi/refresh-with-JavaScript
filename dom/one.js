0e938afda887c3f3c4bcf1da614df3db476b3743
ershahidnabi <ershahidnabi@gmail.com>
2024-04-11 10:58:26 +0530

    DOM Completed

---
+++ dom/one.js
@@ -0,0 +1,8 @@
+title.style.border = "2px solid purple";
+title.style.borderRadius = "12px";
+title.style.padding = "12px";
+
+const innerText = title.innerText;
+const textContent = title.textContent;
+const innerHTml = title.innerHTML;
+console.table(innerHTml, textContent, innerText);
